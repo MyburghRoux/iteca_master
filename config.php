5e239cca341b86f0fe878a12aeb09bed5f178319
Carel Myburgh Roux <myburghroux7@gmail.com>
2024-06-22 20:03:10 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $currency = 'R';
-$db_username = 'root';
-$db_password = '';
-$db_name = 'fusion';
+$db_username = 'nkocnddr_admin';
+$db_password = 'AlbinoLeveesResellTears65';
+$db_name = 'nkocnddr_fusion';
 $db_host = 'localhost';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 ?>
